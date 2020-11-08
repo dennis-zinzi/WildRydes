8ee7e0e5c4bedc58cf944dc6482888f7e52b065a
dennis-zinzi <dennis_zinzi@live.com>
2020-11-08 18:18:07 +0000

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-// Add your API invoke URL below, configured in Module 3. 
+// Add your API invoke URL below, configured in Module 3.
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://y5xh2ecagg.execute-api.eu-central-1.amazonaws.com/production' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
